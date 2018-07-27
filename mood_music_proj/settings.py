5fb37347047cdbd0bcfff4b3d591fc2ae8dcb061
pietromoretto <pietromoretto7@gmail.com>
2018-07-27 15:23:02 +0200

    Secret key fixed

--- mood_music_proj/settings.py
+++ mood_music_proj/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-from .secrets import SECRET_KEY
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -21,6 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'opjkw8p2*g+co#fo(+j-&t6aodu4u)^cbx7ytj$6^4635q#@r+'
 
 ALLOWED_HOSTS = ['moodmusic.pythonanywhere.com', '127.0.0.1']
 
